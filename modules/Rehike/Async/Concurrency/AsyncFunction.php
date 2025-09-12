dd1db7513300ff15f5865df1e5c10578dd4f0bbb
Pumpkin <230778645+lemon-pumpkin-pie@users.noreply.github.com>
2025-09-12 16:42:07 +0900

    Update Rehike library to Rehike/Rehike@3cbda26

--- modules/Rehike/Async/Concurrency/AsyncFunction.php
+++ modules/Rehike/Async/Concurrency/AsyncFunction.php
@@ -126,9 +126,7 @@ class AsyncFunction
                 // getting to us (the network library can do this internally), then
                 // it will no have return value and will throw an exception. We
                 // just have to ignore it.
-                //
-                // TODO: Reconcile differences later as this change was submitted
-                // to Rehike.
+                // PATCH (izzy): Resolved issue #682, thanks lemon-pumpkin-pie!
                 if ($e instanceof Exception
                     && $e->getMessage() == "Cannot get return value of a generator that hasn't returned"
                 )
