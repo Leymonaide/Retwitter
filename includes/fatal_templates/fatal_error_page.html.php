35545136ce7c537a84300a1182900e8fd1847bc1
Isabella Lulamoon <kawapure@gmail.com>
2025-10-12 05:34:47 -0700

    Improve uncaught Promise::All exception error screen
    Signed-off-by: Isabella Lulamoon <kawapure@gmail.com>

--- includes/fatal_templates/fatal_error_page.html.php
+++ includes/fatal_templates/fatal_error_page.html.php
@@ -4,6 +4,7 @@
     use Rehike\ErrorHandler\ErrorHandler;
     use Rehike\ErrorHandler\ErrorPage\FailedToWriteConfigPage;
     use Rehike\ErrorHandler\ErrorPage\UncaughtExceptionPage;
+    use Rehike\ErrorHandler\ErrorPage\PromiseAllExceptionPage;
     use Rehike\ErrorHandler\ErrorPage\InnertubeFailedRequestPage;
     use Rehike\ErrorHandler\ErrorPage\FatalErrorPage;
 
@@ -108,6 +109,13 @@
                 <?php endif ?>
 
                 <?php if ($page instanceof UncaughtExceptionPage): ?>
+                    <?php if ($page instanceof PromiseAllExceptionPage): ?>
+                        <h2>Inner exception:</h2>
+                        <pre class="inner-exception-log exception-log"><?=
+                            simpleFormattedStringToHtml($page->getInnerExceptionLog())
+                        ?></pre>
+                        <h2>Outer exception:</h2>
+                    <?php endif ?>
                     <pre class="exception-log"><?= 
                         simpleFormattedStringToHtml($page->getExceptionLog())
                     ?></pre>
