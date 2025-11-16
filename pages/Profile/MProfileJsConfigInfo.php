5d750413c56140ccf65a3637d442f2f95ce69623
Pumpkin <230778645+lemon-pumpkin-pie@users.noreply.github.com>
2025-11-16 20:52:10 +0900

    Report profile URL to profile JS config

--- pages/Profile/MProfileJsConfigInfo.php
+++ pages/Profile/MProfileJsConfigInfo.php
@@ -54,7 +54,7 @@ class MProfileJsConfigInfo
         $this->name = $parser->getUsername() ?? "";
         $this->screenName = $parser->getDisplayName() ?? $this->name;
         $this->location = $parser->getLocation() ?? "";
-        //$this->url = $parser->getUrl(); // TODO.
+        $this->url = $parser->getUrlParser()->getTargetUrl();
         $this->protected = $parser->getProtected();
         $this->followersCount = $parser->getFollowerCount() ?? 0;
         $this->friendsCount = $parser->getFollowingCount() ?? 0;
