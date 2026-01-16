f884d2254b480986ee5096ca53b91a188d689888
aubymori <aubyomori@gmail.com>
2026-01-15 18:26:49 -0600

    Implement TwitterInitialDocument caching Like nameserver cache, it lasts 5 hours.

--- modules/Retwitter/TwitterInitialDocument.php
+++ modules/Retwitter/TwitterInitialDocument.php
@@ -22,6 +22,9 @@ namespace Retwitter;
 
 use PHPHtmlParser\Dom;
 use Rehike\Async\Promise;
+use Rehike\Exception\FileSystem\FsFileDoesNotExistException;
+use Rehike\FileSystem;
+use Rehike\Logging\DebugLogger;
 use Rehike\Network\NetworkCore;
 use Retwitter\Utils\NitterParsingUtils;
 
@@ -37,6 +40,20 @@ use const Retwitter\Constants\FEATURE_SIGNIN;
  */
 class TwitterInitialDocument
 {
+    /**
+     * The path to the file used to store the cache.
+     * 
+     * @var string
+     */
+    private const CACHE_FILE = "cache/initial_doc_cache.json";
+
+    /**
+     * The amount of time for which a cache entry is valid.
+     * 
+     * @var int
+     */
+    private const CACHE_VALID_TIME = 18000; // 5 hours
+
     private static self $instance;
     
     public static function getInstance(): static
@@ -51,32 +68,98 @@ class TwitterInitialDocument
     
     private ?string $document = null;
     private ?Dom $dom = null;
+
+    private function readFromCache(): ?string
+    {
+        if (!FileSystem::fileExists(self::CACHE_FILE))
+        {
+            return null;
+        }
+
+        try
+        {
+            $jsonStr = FileSystem::getFileContents(self::CACHE_FILE);
+        }
+        catch (FsFileDoesNotExistException $e)
+        {
+            DebugLogger::print(
+                "[TwitterInitialDocument] Cache file stopped existing after check???");
+        }
+
+        if (!is_string($jsonStr))
+        {
+            DebugLogger::print("Failed to read initial document cache file.");
+            return null;
+        }
+
+        $data = json_decode($jsonStr);
+
+        if (!is_object($data) || !isset($data->document) || !isset($data->expire))
+        {
+            DebugLogger::print("Initial document cache file contains invalid data. The file will be removed.");
+            unlink(self::CACHE_FILE);
+            return null;
+        }
+
+        if (time() > $data->expire)
+        {
+            DebugLogger::print("Initial document cache expired.");
+            unlink(self::CACHE_FILE);
+            return null;
+        }
+
+        return $data->document;
+    }
+
+    /**
+     * Purges the initial document cache.
+     * This method should probably be called upon any action that changes
+     * the signin state (log in, log out).
+     */
+    public function purgeCache(): void
+    {
+        unlink(self::CACHE_FILE);
+    }
     
     public function ensure(): Promise
     {
         return async(function()
         {
 if (!TEST_SIGNIN):
-            $response = yield NetworkCore::request("https://x.com", [
-                "headers" => [
-                    "User-Agent" => $_SERVER["HTTP_USER_AGENT"],
-                    // Pass all user cookies in order to get their logged in __INITIAL_STATE__
-                    "Cookie" => Network::getCurrentRequestCookie(),
-                    "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                    "Accept-Language" => "en",
-                    "Cache-Control" => "no-cache",
-                    "Pragma" => "no-cache",
-                    "Priority" => "u=0, i",
-                    "Sec-Fetch-Mode" => "navigate",
-                    "Sec-Fetch-Dest" => "document",
-                    "Sec-Fetch-Site" => "none",
-                    "Sec-Fetch-User" => "?1",
-                    "Upgrade-Insecure-Requests" => "1",
-                ],
-                "dnsOverride" => Network::DNS_OVERRIDE_HOST,
-            ]);
-            
-            $this->document = $response->getText();
+            $cachedDoc = $this->readFromCache();
+            if ($cachedDoc !== null)
+            {
+                $this->document = $cachedDoc;
+            }
+            else
+            {
+                $response = yield NetworkCore::request("https://x.com", [
+                    "headers" => [
+                        "User-Agent" => $_SERVER["HTTP_USER_AGENT"],
+                        // Pass all user cookies in order to get their logged in __INITIAL_STATE__
+                        "Cookie" => Network::getCurrentRequestCookie(),
+                        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                        "Accept-Language" => "en",
+                        "Cache-Control" => "no-cache",
+                        "Pragma" => "no-cache",
+                        "Priority" => "u=0, i",
+                        "Sec-Fetch-Mode" => "navigate",
+                        "Sec-Fetch-Dest" => "document",
+                        "Sec-Fetch-Site" => "none",
+                        "Sec-Fetch-User" => "?1",
+                        "Upgrade-Insecure-Requests" => "1",
+                    ],
+                    "dnsOverride" => Network::DNS_OVERRIDE_HOST,
+                ]);
+                
+                $this->document = $response->getText();
+
+                // Cache the result to not make excessive requests to x.com
+                $cache = (object)[];
+                $cache->expire = time() + self::CACHE_VALID_TIME;
+                $cache->document = $this->document;
+                FileSystem::writeFile(self::CACHE_FILE, json_encode($cache));
+            }
             \Rehike\Logging\DebugLogger::print("Initial document text: %s", $this->document);
 else:
             $this->document = file_get_contents("cache/test_initialdoc.html");
